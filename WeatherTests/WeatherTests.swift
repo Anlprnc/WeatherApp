186566886f47488fd35119af3588ab32062fecdb
Anlprnc <anilpirincciii@gmail.com>
2024-09-25 14:43:16 +0300

    Initial Commit

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Anıl on 25.09.2024.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
