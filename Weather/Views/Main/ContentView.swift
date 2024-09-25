61fc34f219010a5797a013831e0ddcba78e68d52
Anlprnc <anilpirincciii@gmail.com>
2024-09-25 16:18:09 +0300

    shapes

---
+++ Weather/Views/Main/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Anıl on 25.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/Views/Navigation/TabBar.swift
@@ -0,0 +1,57 @@
+//
+//  TabBar.swift
+//  Weather
+//
+//  Created by Anıl on 25.09.2024.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var action: () -> Void
+    
+    var body: some View {
+        ZStack {
+            // MARK: Arch Shape
+            Arc()
+                .fill(Color.tabBarBackground)
+                .frame(height: 88)
+                .overlay {
+                    // MARK: Arc Border
+                    Arc()
+                        .stroke(Color.customTabBarBorder, lineWidth: 0.5)
+                }
+            
+            // MARK: Tab Items
+            HStack {
+                // MARK: Expand Button
+                Button {
+                    action()
+                } label: {
+                    Image(systemName: "mappin.and.ellipse")
+                        .frame(width: 44, height: 44)
+                }
+                
+                Spacer()
+                
+                // MARK: Navigation Button
+                NavigationLink {
+                    
+                } label: {
+                    Image(systemName: "list.star")
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .font(.title2)
+            .foregroundStyle(.white)
+            .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
+        }
+        .frame(maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    TabBar(action: {})
+        .preferredColorScheme(.dark)
+}
